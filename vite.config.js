0ac0575ae53d5d90d333aeb148508d403f7f8aee
Shivani1228 <tyagishivani1228@gmail.com>
2026-07-10 16:13:16 +0530

    Updated Vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// 🌟 Flowbite React ke content files ko track karne ke liye plugin ko import kiya
-import flowbite from "flowbite-react/tailwind"
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // 🌟 Flowbite React ke content files ko track karne ke liye plugin ko import kiya
+// import flowbite from "flowbite-react/tailwind"
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     flowbite.plugin() // 🌟 Flowbite ka plugin yahan jod diya hai
+//   ],
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    flowbite.plugin() // 🌟 Flowbite ka plugin yahan jod diya hai
-  ],
-})
-
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    // This tells Vite not to look for the missing ./tailwind specifier inside flowbite-react
+    exclude: ["flowbite-react"],
+  },
+});
